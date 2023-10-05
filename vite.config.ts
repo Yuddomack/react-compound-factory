d757e54eac719580814563aff8bf9377e0d38bb4
yuddomack <darr880406@gmail.com>
2023-10-05 17:24:15 +0900

    change build option

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
 import react from "@vitejs/plugin-react"
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ entryRoot: "src" })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
       name: "react-coumpound-factory",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "react",
+        },
+      },
     },
   },
 })
