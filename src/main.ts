6dd34e4464c8c2eb549a255964ecdd87dc617798
yuddomack <darr880406@gmail.com>
2023-10-05 17:32:21 +0900

    make logic

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,38 @@
-export function some() {
-  return "aa"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Children, isValidElement } from "react"
+
+export const isComponentOf = (
+  component: JSX.Element,
+  constructor: (...args: any) => React.ReactNode,
+) => constructor === component.type
+
+export const getCompoundFactory = <
+  T extends Record<string, React.JSXElementConstructor<any>>,
+>(
+  children: React.ReactNode,
+  map: T,
+) => {
+  const invertMap = new Map<React.JSXElementConstructor<any>, keyof T>()
+  Object.entries(map).forEach(([key, value]) => invertMap.set(value, key))
+
+  const factory: Partial<Record<keyof T, React.ReactNode>> = {}
+  const childrenArray = Children.toArray(children)
+
+  childrenArray.forEach((element) => {
+    if (!isValidElement(element)) {
+      return
+    }
+
+    const { type } = element
+    if (typeof type === "string") {
+      return
+    }
+
+    const key = invertMap.get(type)
+    if (key) {
+      factory[key] = element
+    }
+  })
+
+  return factory
 }
